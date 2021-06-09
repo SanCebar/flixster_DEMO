5eec1f90499bed1397085e7d8741b5c3f60361f9
SanCebar <r19velasco@gmail.com>
2021-06-09 15:29:33 -0700

    New Event Handler for search bar

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-/* Global Variables */
-const api_key = "fde7bb13d16debd8e524a7bccdaf1b03";
-const display = document.querySelector(".grid-container");
+/**
+ * Author: Robert Velasco
+ * Date: 06/11/2021
+ * 
+ * Description:
+ */
 
 // 1. Populate screen with movies
 //      - make API call
 // 2. Search functionality
 
-const grid_item_1 = document.querySelector(".grid-item-1");
+/* Global Variables */
+const api_key = "fde7bb13d16debd8e524a7bccdaf1b03";
+const display = document.querySelector(".grid-container");
+const movieForm = document.querySelector("form");
 
+/* Event Handlers */
+movieForm.addEventListener("submit", handleSearch);
+
+
+/**
+ * getMovies - retrieves array of objects from API
+ */
 async function getMovies () {
     console.log("Inside getResults...");
     const apiUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}&language=en-US`;
@@ -15,22 +28,35 @@ async function getMovies () {
     //go to movie API
     const response = await fetch(apiUrl);
     const responseData = await response.json();
-    const data = responseData.data;
-    console.log("Data retreived!");
+    const data = responseData.results;
+    console.log(responseData);
     console.log(data);
 
     //display movies
     displayMovies(data);
 }
 
+/**
+ * displayMovies - displays three different pieces of information for each 
+ *                 movie: poster, title, votes
+ * 
+ * @param {*} data 
+ */
 function displayMovies(data) {
     data.forEach(element => {
         display.innerHTML += `
             <div class="grid-item"> 
+                <img src="http://image.tmdb.org/t/p/w185${element.poster_path}" alt="Movie Poster for ${element.title}">
                 <h4>${element.title}</h4>
+                <h5>Votes: ${element.vote_count}<h5>
             </div>
         `
     }); 
+}
+
+function handleSearch (event) {
+    event.preventDefault();
+    console.log("Inside handleSearch...")
 }
 
 window.onload = function () {
